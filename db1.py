035d4c98d6fed00f4b84baaffab1c65dc37e7cd7
mishraadarsh17 <a177541ten@gmail.com>
2026-05-29 17:07:07 +0530

    fixed user profile columns

--- db1.py
+++ db1.py
@@ -14,14 +14,33 @@ cursor.execute("""
               CREATE TABLE IF NOT EXISTS users
                ( id INTEGER PRIMARY KEY AUTOINCREMENT,
                username TEXT UNIQUE,
-               password TEXT
+               password TEXT,
+               name TEXT,
+               email TEXT,
+               dob TEXT,
+               contact TEXT,
+               profile_photo TEXT
                )
 """)
-
-# cursor.execute("ALTER TABLE users ADD COLUMN name TEXT")
-# cursor.execute("ALTER TABLE users ADD COLUMN email TEXT")
-# cursor.execute("ALTER TABLE users ADD COLUMN dob TEXT")
-# cursor.execute("ALTER TABLE users ADD COLUMN contact TEXT")
-# cursor.execute("ALTER TABLE users ADD COLUMN profile_photo TEXT")
+try:
+  cursor.execute("ALTER TABLE users ADD COLUMN name TEXT")
+except:
+  pass
+try:
+  cursor.execute("ALTER TABLE users ADD COLUMN email TEXT")
+except:
+  pass
+try:
+  cursor.execute("ALTER TABLE users ADD COLUMN dob TEXT")
+except:
+  pass
+try:
+  cursor.execute("ALTER TABLE users ADD COLUMN contact TEXT")
+except:
+  pass
+try:
+  cursor.execute("ALTER TABLE users ADD COLUMN profile_photo TEXT")
+except:
+  pass
 conn.commit()
 conn.close()
